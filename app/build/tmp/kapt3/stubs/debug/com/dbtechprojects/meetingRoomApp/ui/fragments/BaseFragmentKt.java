0a819f1f9ba175fe41360dccbe359ba128be22a4
danielmbutler <danielmbutler@hotmail.co.uk>
2021-04-16 22:49:58 +0100

    initial commit

---
+++ app/build/tmp/kapt3/stubs/debug/com/dbtechprojects/meetingRoomApp/ui/fragments/BaseFragmentKt.java
@@ -0,0 +1,21 @@
+package com.dbtechprojects.meetingRoomApp.ui.fragments;
+
+import java.lang.System;
+
+@kotlin.Metadata(mv = {1, 4, 1}, bv = {1, 0, 3}, k = 2, d1 = {"\u0000$\n\u0000\n\u0002\u0010\u000e\n\u0002\b\u0002\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0003\n\u0002\u0018\u0002\n\u0000\u001a\u0016\u0010\u0003\u001a\u00020\u00042\u0006\u0010\u0005\u001a\u00020\u00062\u0006\u0010\u0007\u001a\u00020\b\u001a&\u0010\t\u001a\u00020\u00042\u0006\u0010\n\u001a\u00020\u00012\u0006\u0010\u0007\u001a\u00020\b2\u0006\u0010\u0005\u001a\u00020\u00062\u0006\u0010\u000b\u001a\u00020\f\"\u000e\u0010\u0000\u001a\u00020\u0001X\u0082T\u00a2\u0006\u0002\n\u0000\"\u000e\u0010\u0002\u001a\u00020\u0001X\u0082T\u00a2\u0006\u0002\n\u0000\u00a8\u0006\r"}, d2 = {"ARG_PARAM1", "", "ARG_PARAM2", "showToastFragment", "", "context", "Landroid/content/Context;", "view", "Landroid/view/View;", "showerror", "error", "layoutInflater", "Landroid/view/LayoutInflater;", "app_debug"})
+public final class BaseFragmentKt {
+    private static final java.lang.String ARG_PARAM1 = "param1";
+    private static final java.lang.String ARG_PARAM2 = "param2";
+    
+    public static final void showToastFragment(@org.jetbrains.annotations.NotNull()
+    android.content.Context context, @org.jetbrains.annotations.NotNull()
+    android.view.View view) {
+    }
+    
+    public static final void showerror(@org.jetbrains.annotations.NotNull()
+    java.lang.String error, @org.jetbrains.annotations.NotNull()
+    android.view.View view, @org.jetbrains.annotations.NotNull()
+    android.content.Context context, @org.jetbrains.annotations.NotNull()
+    android.view.LayoutInflater layoutInflater) {
+    }
+}
